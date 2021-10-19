f52b1124f6b1c481904fea8263917868a5678ecd
Bryan Leow Xuan Zhen <blxz96@gmail.com>
2021-10-19 10:37:34 +0800

    Completed Module 3: Spring Data JPA Repositories
    -Created TicketPriceJpaRepository.java
    -Modify the TicketPriceTest.java by setting autowired to TicketPriceJpaRepository and change syntax to fit into TicketPriceJpaRepository
    -Created SessionJpaRepository.java
    -Modify SessionRepository.java to illustrate proxy strategy and how we do not need to change the SessionTest with proxy strategy

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
@@ -1,6 +1,7 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -9,32 +10,46 @@ import java.util.List;
 
 @Repository
 public class SessionRepository {
+    // Proxy strategy
+    // Doing this allows us to call the jpaRepository and proxying over to that repository rather than
+    //implementing it ourselves, i.e. use the jpaRepository as proxies - used when  refactoring a code.
+    //Don't have to modify any of the controller or session test in this case
+    @Autowired
+    private SessionJpaRepository jpaRepository;
+
     @PersistenceContext
     private EntityManager entityManager;
 
+    //Note saveAndFlush will create/update your entity
+
     public Session create(Session session) {
-        entityManager.persist(session);
-        entityManager.flush();
-        return session;
+//        entityManager.persist(session);
+//        entityManager.flush();
+//        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public Session update(Session session) {
-        session = entityManager.merge(session);
-        entityManager.flush();
-        return session;
+//        session = entityManager.merge(session);
+//        entityManager.flush();
+//        return session;
+        return jpaRepository.saveAndFlush(session);
     }
 
     public void delete(Long id) {
-        entityManager.remove(find(id));
-        entityManager.flush();
+//        entityManager.remove(find(id));
+//        entityManager.flush();
+        jpaRepository.deleteById(id);
     }
 
     public Session find(Long id) {
-        return entityManager.find(Session.class, id);
+//        return entityManager.find(Session.class, id);
+        return jpaRepository.getOne(id);
     }
 
     public List<Session> list() {
-        return entityManager.createQuery("select s from Session s").getResultList();
+//        return entityManager.createQuery("select s from Session s").getResultList();
+        return jpaRepository.findAll();
     }
 
     public List<Session> getSessionsThatHaveName(String name) {
