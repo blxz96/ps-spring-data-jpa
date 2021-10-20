787b703fefc4d87cd18d2bf37bcff84abd0edfd4
Bryan Leow Xuan Zhen <blxz96@gmail.com>
2021-10-20 15:00:11 +0800

    Completed Module 6 : Advanced features -This module covered Paging and Sorting and also building custom respositories
    and also write the respective test cases

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -1,11 +1,15 @@
 package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface SessionJpaRepository extends JpaRepository<Session,Long> {
+public interface SessionJpaRepository extends JpaRepository<Session,Long> , SessionCustomJpaRepository{
     List<Session> findBySessionNameContains (String name);
     List<Session> findBySessionLengthNot(Integer sessionLength);
     List<Session> findBySessionNameNotLike (String name);
@@ -13,5 +17,9 @@ public interface SessionJpaRepository extends JpaRepository<Session,Long> {
     List<Session> findBySessionNameEndingWith (String name);
     List<Session> findBySessionLengthLessThan (Integer length);
     List<Session> findBySessionLengthGreaterThan (Integer length);
+
+    @Query("select  s from Session s where s.sessionName like %:name")
+    Page<Session> getSessionsWithName(@Param("name") String name, Pageable pageable);
+
 
 }
--- conference-demo/src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
+++ conference-demo/src/test/java/com/pluralsight/conferencedemo/models/SessionTest.java
@@ -5,6 +5,9 @@ import com.pluralsight.conferencedemo.repositories.SessionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -58,6 +61,18 @@ public class SessionTest {
     public void testJpaGreaterThan() throws Exception {
         List<Session> sessions = jpaRepository.findBySessionLengthGreaterThan(30);
         assertTrue(sessions.size() > 0);
+    }
+
+    @Test
+    public void testPagingSorting() throws Exception{
+        Page<Session> page = jpaRepository.getSessionsWithName("S", PageRequest.of(1,5,Sort.by(Sort.Direction.DESC,"sessionLength")));
+        assertTrue(page.getTotalElements()>0);
+    }
+
+    @Test
+    public void testCustomImpl() throws Exception{
+        List<Session> sessions = jpaRepository.customGetSessions();
+        assertTrue(sessions.size()>0);
     }
 
 }
